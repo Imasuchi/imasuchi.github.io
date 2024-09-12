2066187141ceb3afa8b36ffbc8e1fb13613c060e
Imasuchi <150727456+Imasuchi@users.noreply.github.com>
2024-09-12 11:39:30 -0500

    Update austin.js

--- austin.js
+++ austin.js
@@ -4,16 +4,16 @@ const shuffleBtn = document.getElementById('shuffle-btn');
 const nextBtn = document.getElementById('next-btn');
 const spinImage = document.getElementById('spin-image');
 const songs = [
-    { name: "From the Start", src: "songs/From the Start.mp3", img: "records/from-the-start.png" },
-    { name: "The Bird Song", src: "songs/The Bird Song.mp3", img: "records/the-bird-song.png" },
-    { name: "Seventh Heaven", src: "songs/Seventh Heaven.mp3", img: "records/seventh-heaven.png" },
-    { name: "Not Like Us", src: "songs/Not Like Us.mp3", img: "records/not-like-us.png" },
-    { name: "Mimis Delivery Service", src: "songs/Mimis Delivery Service.mp3", img: "records/mimis-delivery-service.png" },
-    { name: "Vacancy!", src: "songs/Vacancy!.mp3", img: "records/vacancy.png" },  
-    { name: "Pointless relations", src: "songs/Pointless relations.mp3", img: "records/Pointless-Relations.png" },
-    { name: "I WANNA BE", src: "songs/I WANNA BE.mp3", img: "records/i-wanna-be.png" },  
-    { name: "Come tell me the real way! (feat. Taiketsu)", src: "songs/Come tell me the real way! (feat. Taiketsu).mp3", img: "records/Come-tell-me-the-real-way!-(feat.-Taiketsu).png" },
-    { name: "spectacular", src: "songs/spectacular.mp3", img: "records/spectacular.png" }
+    { name: "From the Start", src: "songs/From the Start.ogg", img: "records/from-the-start.png" },
+    { name: "The Bird Song", src: "songs/The Bird Song.ogg", img: "records/the-bird-song.png" },
+    { name: "Seventh Heaven", src: "songs/Seventh Heaven.ogg", img: "records/seventh-heaven.png" },
+    { name: "Not Like Us", src: "songs/Not Like Us.ogg", img: "records/not-like-us.png" },
+    { name: "Mimis Delivery Service", src: "songs/Mimis Delivery Service.ogg", img: "records/mimis-delivery-service.png" },
+    { name: "Vacancy!", src: "songs/Vacancy!.ogg", img: "records/vacancy.png" },  
+    { name: "Pointless relations", src: "songs/Pointless relations.ogg", img: "records/Pointless-Relations.png" },
+    { name: "I WANNA BE", src: "songs/I WANNA BE.ogg", img: "records/i-wanna-be.png" },  
+    { name: "Come tell me the real way! (feat. Taiketsu)", src: "songs/Come tell me the real way! (feat. Taiketsu).ogg", img: "records/Come-tell-me-the-real-way!-(feat.-Taiketsu).png" },
+    { name: "spectacular", src: "songs/spectacular.ogg", img: "records/spectacular.png" }
 ];
 let currentSongIndex = 0;
 let playedSongs = [];
