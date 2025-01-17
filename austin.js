ecaf65339bc68dcecd09978b2e7d9a256a6c4cc4
Imasuchi <150727456+Imasuchi@users.noreply.github.com>
2025-01-17 13:57:26 -0600

    Update austin.js

--- austin.js
+++ austin.js
@@ -50,7 +50,8 @@ const songs = [
 { name: "Buddy Holly", src: "songs/Buddy Holly.ogg", img: "records/Austin.gif" },
 { name: "GOSSIP", src: "songs/GOSSIP (feat. Tom Morello).ogg", img: "records/Austin.gif" },
 { name: "Tadow", src: "songs/Tadow.ogg", img: "records/Austin.gif" },
-{ name: "Come as You are", src: "songs/Come as you are (online-audio-converter.com).ogg", img: "records/Austin.gif" }
+{ name: "Come as You are", src: "songs/Come as you are (online-audio-converter.com).ogg", img: "records/Austin.gif" },
+{ name: "Faster car", src: "songs/Faster car.ogg", img: "records/Austin.gif" }
    
 ];
 let currentSongIndex = 0;
